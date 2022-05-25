1a59664222c338f8b45db79e484d3e1611aefe40
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2022-05-25 22:50:04 +0530

    Update 181. Employees Earning More Than Their Managers.sql

--- Leetcode SQL/181. Employees Earning More Than Their Managers.sql
+++ Leetcode SQL/181. Employees Earning More Than Their Managers.sql
@@ -3,3 +3,8 @@ select T1.name as Employee
 from Employee T1
 join Employee T2
 on T1.managerId=T2.id and T1.salary>T2.salary
+
+# 2nd Approach
+
+Select e1.name as "Employee" from Employee as e1 
+where e1.salary > (Select salary from Employee where id= e1.managerId);
