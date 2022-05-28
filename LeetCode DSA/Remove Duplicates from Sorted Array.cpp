ada393bdb46806a8debe554a25b50804d54ae61b
Nitesh Ranjan Singh <63156061+nitesh0007-edith@users.noreply.github.com>
2022-05-28 22:52:00 +0530

    Create Remove Duplicates from Sorted Array.cpp

---
+++ LeetCode DSA/Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+      int r=1;
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i]!=nums[r-1])
+            {
+                nums[r]=nums[i];
+                r++;
+            }
+        }
+        return r;
+    }
+    
+    
+};
